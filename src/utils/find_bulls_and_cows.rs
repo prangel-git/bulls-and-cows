5b8d818c27e20994d35d915d72af54430d6aa99b
prangel-git <62568827+prangel-git@users.noreply.github.com>
2023-01-18 15:32:55 -0500

    replacing find_bulls_and_cows

--- src/utils/find_bulls_and_cows.rs
+++ src/utils/find_bulls_and_cows.rs
@@ -1,8 +1,8 @@
-use crate::{CodeDigits, Clue};
+use crate::{CodeDigits, Clue, CODELENGTH};
 
 use super::find_matching::find_matching;
 
-pub fn find_bulls_and_cows(secret: &CodeDigits, guess: &CodeDigits) -> Clue {
+pub fn _find_bulls_and_cows_1(secret: &CodeDigits, guess: &CodeDigits) -> Clue {
     let mut bulls = 0;
     let mut cows = 0;
     
@@ -16,6 +16,32 @@ pub fn find_bulls_and_cows(secret: &CodeDigits, guess: &CodeDigits) -> Clue {
         }
     }
     
+    Clue {bulls, cows}
+}
+
+pub fn find_bulls_and_cows(secret: &CodeDigits, guess: &CodeDigits) -> Clue {
+    let mut bulls = 0;
+    let mut cows = 0;
+
+    let mut index_secret = [0u8; 10];
+    let mut index_guess = [0u8; 10];
+
+    for i in 0..CODELENGTH {
+        let secret_i = secret[i];
+        let guess_i = guess[i];
+
+        if secret_i == guess_i {
+            bulls += 1;
+        } else {
+            index_secret[secret_i as usize] += 1;
+            index_guess[guess_i as usize] += 1;
+        }
+    }
+
+    for i in 0..CODELENGTH {
+        cows += std::cmp::min(index_secret[i], index_guess[i]); 
+    }
+
     Clue {bulls, cows}
 }
 

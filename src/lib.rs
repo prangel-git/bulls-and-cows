c99d54e087d9c9f3bb4e58aa8b54f3b15f2970c5
prangel-git <62568827+prangel-git@users.noreply.github.com>
2023-01-18 01:02:31 -0500

    Adding tests for find_matching.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 type Digit = i8;
 type CodeDigits = [Digit; 4];
 type IndexToIndexMap = HashMap<usize, usize>;
+type IndexSet = HashSet<usize>;
 
 pub mod utils;
--- src/utils/find_matching.rs
+++ src/utils/find_matching.rs
@@ -1,12 +1,15 @@
-use crate::{CodeDigits, IndexToIndexMap};
+use crate::{CodeDigits, IndexToIndexMap, IndexSet};
 
 pub fn find_matching(code_a: CodeDigits, code_b: CodeDigits) -> IndexToIndexMap {
     let mut matching = IndexToIndexMap::new();
+    let mut used_indexes = IndexSet::new();
     
     for i in 0..4 {
         for j in 0..4 {
-            if code_a[i] == code_b[j] {
+            if code_a[i] == code_b[j] && !used_indexes.contains(&j) {
                 matching.insert(i, j);
+                used_indexes.insert(i);
+                break;
             }
         }        
     }
@@ -28,6 +31,30 @@ mod tests {
             (1,1),
             (2,2),
             (3,3)
+        ]);
+
+        assert_eq!(expected_answer, find_matching(code_a, code_b));
+    }
+
+    #[test]
+    fn find_matching_between_0123_and_0000() {
+        let code_a = [0, 1, 2, 3];
+        let code_b = [0, 0, 0, 0];
+
+        let expected_answer = IndexToIndexMap::from([
+            (0,0)
+        ]);
+
+        assert_eq!(expected_answer, find_matching(code_a, code_b));
+    }
+
+    #[test]
+    fn find_matching_between_0000_and_0123() {
+        let code_a = [0, 0, 0, 0];
+        let code_b = [0, 1, 2, 3];
+
+        let expected_answer = IndexToIndexMap::from([
+            (0,0)
         ]);
 
         assert_eq!(expected_answer, find_matching(code_a, code_b));

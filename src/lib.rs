6cbbda1c2b8a6d8b990dfdb5fcd6f383c4a477c9
prangel-git <62568827+prangel-git@users.noreply.github.com>
2023-01-18 15:12:57 -0500

    making priviate some types.

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,8 @@ const CODELENGTH: usize = 4;
 const CODEMAX: u16 = 10usize.pow(CODELENGTH as u32) as u16;
 
 type Digit = u8;
-pub type CodeDigits = [Digit; CODELENGTH];
-pub type CodeWord = u16;
+type CodeDigits = [Digit; CODELENGTH];
+type CodeWord = u16;
 type IndexToIndexMap = HashMap<usize, usize>;
 type IndexSet = HashSet<usize>;
 

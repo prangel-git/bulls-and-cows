57d933f29d92f6bd3d8dfafee0e6165a594fc4d8
prangel-git <62568827+prangel-git@users.noreply.github.com>
2023-01-18 12:05:40 -0500

    eliminating magic number.

--- src/knuth/test_all_secrets.rs
+++ src/knuth/test_all_secrets.rs
@@ -1,13 +1,14 @@
-use crate::{CodeWord, CODEMAX, utils::codeword_to_codedigits};
+use crate::{CodeWord, utils::codeword_to_codedigits};
 
 use super::play_against_knuth;
 type NumAttempts = u8;
 
+const CODEMAX: u16 = 1000;
 
 pub fn test_all_secrets() -> Vec<(CodeWord, NumAttempts)> {
     let mut secret_vs_attempts = Vec::new();
 
-    for codeword in 0..1000 {
+    for codeword in 0..CODEMAX {
         let secret = codeword_to_codedigits(codeword);
         let log = play_against_knuth(&secret);
         secret_vs_attempts.push((codeword, log.len() as NumAttempts));

ce53cfda763adf80d39a0f8abaa44e96587362ef
prangel-git <62568827+prangel-git@users.noreply.github.com>
2023-01-18 10:50:39 -0500

    adding get guess.

--- src/knuth/mindstate.rs
+++ src/knuth/mindstate.rs
@@ -13,6 +13,10 @@ pub fn update_mindstate(guess: &CodeDigits, clue: &Clue, mindstate: &mut MindSta
     mindstate.retain(|secret| find_bulls_and_cows(secret, guess) == *clue);  
 }
 
+pub fn get_guess(mindstate: &MindState) -> CodeDigits {
+    return mindstate[0].clone()
+}
+
 #[cfg(test)]
 mod tests{
     use super::*;
@@ -32,4 +36,26 @@ mod tests{
         assert!(mindstate.contains(&[0,1,2,3]));
         assert_eq!(mindstate.len(), 1)   
     }
+
+    #[test]
+    fn guess_0123_clue_0b4c_returns_mindstate_containing_9elements() {
+        let mut mindstate = new();
+        let guess = [0,1,2,3];
+        let clue = Clue{bulls:0, cows:4};
+        update_mindstate(&guess, &clue, &mut mindstate);
+
+        assert_eq!(mindstate.len(), 9)
+    }
+
+    #[test]
+    fn get_guess_returns_first_valid_guess() {
+        let mut mindstate = new();
+        let guess = [0,1,2,3];
+        let clue = Clue{bulls:0, cows:4};
+        update_mindstate(&guess, &clue, &mut mindstate);
+
+        assert_eq!(get_guess(&mindstate), [1,0,3,2])
+    }
+
+    
 }
